515ca84510edd0707c77523b81616ab0744f5570
Barella Riccardo <rbarella@eosao45.eurac.edu>
2025-09-17 11:39:49 +0200

    demo version ready

--- run_micromet_DEMO_MAIPO.sh
+++ run_micromet_DEMO_MAIPO.sh
@@ -10,7 +10,7 @@ conda activate microenv
 
 # Path to your Python script and config
 SCRIPT_PATH="./main_micromet.py"
-CONFIG_PATH="./micro_config_MAIPO_AUTODEM.json"
+CONFIG_PATH="./micro_config_DEMO_MAIPO.json"
 
 echo "Running MicroMet downscaling..."
 python "$SCRIPT_PATH" "$CONFIG_PATH"
